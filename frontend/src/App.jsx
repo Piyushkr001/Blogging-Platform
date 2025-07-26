dfc2632e4e3af5af297437d3620a51238db8bfeb
Piyushkr001 <piyushthegreat2305@gmail.com>
2025-07-26 20:45:59 +0530

    `Added lucide-react dependency and updated frontend routes`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 import ForgotPasswordPage from './pages/ForgotPassword';
 import { Toaster } from 'react-hot-toast';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoutes';
+import DashboardHome from './pages/DashboardHome';
 
 function App() {
   return (
@@ -19,6 +22,21 @@ function App() {
             <Route path='/login' element={<LoginPage />} />
             <Route path='/register' element={<RegisterPage />} />
             <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+
+
+           <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            >
+              {/* ✅ Nested routes inside Dashboard layout */}
+               <Route index element={<DashboardHome />} />
+
+            </Route>
+
            </Routes>
          </div>
            <Footer />
